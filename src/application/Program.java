f5e0f19cfb79a33f52746716c9238c942c2205b5
Rodrigo Alexandre <rodrigo11.vgp@gmail.com>
2026-02-25 15:51:23 -0300

    Initial commit

---
+++ src/application/Program.java
@@ -0,0 +1,86 @@
+/*
+* Fazer um programa para ler os dados de N
+produtos (N fornecido pelo usuário). Ao final,
+mostrar a etiqueta de preço de cada produto na
+mesma ordem em que foram digitados.
+*
+Todo produto possui nome e preço. Produtos
+importados possuem uma taxa de alfândega, e
+produtos usados possuem data de fabricação.
+*
+Estes dados específicos devem ser
+acrescentados na etiqueta de preço conforme
+exemplo (próxima página). Para produtos
+importados, a taxa e alfândega deve ser
+acrescentada ao preço final do produto.
+* */
+
+package application;
+
+import entities.ImportedProduct;
+import entities.Product;
+import entities.usedProduct;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        List<Product> products = new ArrayList<>();
+
+        System.out.print("Enter the number of products: ");
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            sc.nextLine();
+            System.out.println("Product #" + (i + 1) + " data: ");
+            System.out.print("Common, used or imported (c/u/i)? ");
+            String resp = sc.nextLine();
+            if (resp.equals("c")) {
+                System.out.print("Name: ");
+                String name = sc.nextLine();
+                System.out.print("Price: ");
+                Double price = sc.nextDouble();
+                Product common = new Product(name, price);
+                products.add(common);
+            } else if (resp.equals("u")) {
+                System.out.print("Name: ");
+                String name = sc.nextLine();
+                System.out.print("Price: ");
+                Double price = sc.nextDouble();
+                sc.nextLine();
+                System.out.print("Manufacture Date (DD/MM/YYYY): ");
+                String dateStr = sc.nextLine();
+                LocalDate manufactureDate = LocalDate.parse(dateStr, fmt);
+                Product used = new usedProduct(name, price, manufactureDate);
+                products.add(used);
+            } else if (resp.equals("i")) {
+                System.out.print("Name: ");
+                String name = sc.nextLine();
+                System.out.print("Price: ");
+                Double price = sc.nextDouble();
+                System.out.print("Customs fee: ");
+                Double customFee = sc.nextDouble();
+                Product imported = new ImportedProduct(name, price, customFee);
+                products.add(imported);
+            }
+        }
+
+        System.out.println("\n----------------------------------------------------");
+        System.out.println("\nPRICE TAGS: ");
+
+        for (Product p : products) {
+            System.out.println(p.priceTag());
+        }
+
+        sc.close();
+    }
+}
---
+++ src/entities/ImportedProduct.java
@@ -0,0 +1,32 @@
+package entities;
+
+public class ImportedProduct extends Product{
+    private Double customFee;
+
+    public ImportedProduct(String name, Double price, Double customFee) {
+        super(name, price);
+        this.customFee = customFee;
+    }
+
+    public Double getCustomFee() {
+        return customFee;
+    }
+
+    public void setCustomFee(Double customFee) {
+        this.customFee = customFee;
+    }
+
+    public Double totalPrice() {
+        return super.getPrice() + customFee;
+    }
+
+    @Override
+    public String priceTag() {
+        return "Nome: " + getName()
+                + " | Valor: $"
+                + String.format("%.2f", totalPrice())
+                + " | (Customs fee: $"
+                + String.format("%.2f", customFee)
+                + ")";
+    }
+}
