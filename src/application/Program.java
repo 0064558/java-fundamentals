72bb18d07f0fc7096a76eb25e79ebbafe0797c04
Rodrigo Alexandre <rodrigo11.vgp@gmail.com>
2026-03-03 21:38:31 -0300

    Initial commit

---
+++ src/application/Program.java
@@ -0,0 +1,57 @@
+/*
+* Uma locadora brasileira de carros cobra um valor por hora para locações de até
+12 horas. Porém, se a duração da locação ultrapassar 12 horas, a locação será
+cobrada com base em um valor diário. Além do valor da locação, é acrescido no
+preço o valor do imposto conforme regras do país que, no caso do Brasil, é 20%
+para valores até 100.00, ou 15% para valores acima de 100.00. Fazer um
+programa que lê os dados da locação (modelo do carro, instante inicial e final da
+locação), bem como o valor por hora e o valor diário de locação. O programa
+deve então gerar a nota de pagamento (contendo valor da locação, valor do
+imposto e valor total do pagamento) e informar os dados na tela.
+* */
+
+
+package application;
+
+import model.entities.CarRental;
+import model.entities.Vehicle;
+import model.services.BrazilTaxService;
+import model.services.RentalService;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+
+        System.out.println("Entre com os dados do aluguel: ");
+        System.out.print("Modelo do carro: ");
+        String model = sc.nextLine();
+
+        System.out.print("Retirada (dd/MM/yyy hh:ss): ");
+        LocalDateTime start = LocalDateTime.parse(sc.nextLine(), fmt);
+        System.out.print("Retorno (dd/MM/yyy hh:ss): ");
+        LocalDateTime finish = LocalDateTime.parse(sc.nextLine(), fmt);
+        CarRental carRental = new CarRental(new Vehicle(model), start, finish);
+
+        System.out.print("Entre com o preço por hora: ");
+        double pricePerHour = sc.nextDouble();
+        System.out.print("Entre com o preço por dia: ");
+        double pricePerDay = sc.nextDouble();
+
+        RentalService rentalService = new RentalService(pricePerHour, pricePerDay, new BrazilTaxService());
+
+        rentalService.processInvoice(carRental);
+
+        System.out.println("\n\nFATURA: ");
+        System.out.println("Pagamento básico: " + String.format("%.2f", carRental.getInvoice().getBasicPayment()));
+        System.out.println("Imposto: " + String.format("%.2f", carRental.getInvoice().getTax()));
+        System.out.println("Pagamento total: " + String.format("%.2f", carRental.getInvoice().getTotalPayment()));
+
+        sc.close();
+    }
+}
