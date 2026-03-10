c83b08b2e797b10a37883bd5cda2e1b7dad40810
Rodrigo Alexandre <rodrigo11.vgp@gmail.com>
2026-03-10 14:19:46 -0300

    Add on demo-dao-jdbc Seller entity class

--- demo-dao-jdbc/src/main/java/application/Program.java
+++ demo-dao-jdbc/src/main/java/application/Program.java
@@ -1,11 +1,18 @@
-package program;
+package application;
 
-import db.DB;
 import model.entities.Department;
+import model.entities.Seller;
 
-public class Application {
+import java.util.Date;
+
+public class Program {
     public static void main(String[] args) {
+
         Department dep = new Department(1, "Books");
         System.out.println(dep);
+
+
+        Seller seller = new Seller(1, "Jaspier Purple", "jaspier@gmail.com", new Date(), 3000.0, dep);
+        System.out.println(seller);
     }
 }
